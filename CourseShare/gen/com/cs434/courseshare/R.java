b74ba55dcbe9f9b2b0841526045307ea340b8fe7
px.peter.xu <px.peter.xu@gmail.com>
2012-12-03 17:08:23 -0500

    Created main NavigationActivity with buttons for each function.

--- CourseShare/gen/com/cs434/courseshare/R.java
+++ CourseShare/gen/com/cs434/courseshare/R.java
@@ -18,7 +18,7 @@ public final class R {
         public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_entry=0x7f030000;
+        public static final int activity_navigation=0x7f030000;
     }
     public static final class menu {
         public static final int activity_entry=0x7f060000;
@@ -27,7 +27,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
-        public static final int title_activity_entry=0x7f040003;
+        public static final int title_activity_navigation=0x7f040003;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
